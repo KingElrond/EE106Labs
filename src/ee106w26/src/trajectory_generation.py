00552a39e3c8254b7bf04d4fd4e1f7bd560e2ab3
KingElrond <tdres002@DESKTOP-R90T8ON.localdomain>
2026-03-01 21:03:05 -0800

    made progress. trajectory generation might be done

--- src/ee106w26/src/trajectory_generation.py
+++ src/ee106w26/src/trajectory_generation.py
@@ -9,6 +9,10 @@ from std_msgs.msg import Empty
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist, Pose2D
 
+def euclid_dist(candidate, goal):
+    #manhattan Distance
+    return sqrt((abs(candidate[0] - goal[0]))**2 + (abs(candidate[1] - goal[1]))**2)
+
 class Controller: #PID controller from lab 4
     def __init__(self, P=0.0, D=0.0, set_point=0):
         self.Kp = P
@@ -80,9 +84,115 @@ class Turtlebot():
     def move_to_point(self, current_waypoint, next_waypoint):
         # generate polynomial trajectory and move to current_waypoint
         # next_waypoint is to help determine the velocity to pass current_waypoint
+        lv_max = 0.22 #max linear speed
+        av_max = 2.0 #max angular speed
+        cont = Controller(0.9,0.1)
+        gx, gy = current_waypoint
+        nx, ny = next_waypoint
+        #get pose from Pose2D
+        x, y, th = self.pose.x, self.pose.y, self.pose.theta
+        #get dist
+        disX = gx-x
+        disY = gy-y
+        disNX = nx - gx
+        disNY = ny - gy
+        dist = euclid_dist((x,y),current_waypoint)
+        distN = euclid_dist(current_waypoint, next_waypoint)
         
+        gth = atan2(disY, disX)
+        ngth = atan2((ny-gy), (nx-gx))
+        nturn = ngth - gth
+        while nturn > pi:
+            nturn -= 2*pi
+        while nturn < -pi:
+            nturn += 2*pi
         
-        pass
+        turn_abs = abs(nturn)
+
+        # map [0 .. pi/2] -> [1 .. 0] for adjusting turn end speeds.
+        if turn_abs >= (pi/2):
+            turn_scale = 0.0
+        else:
+            turn_scale = ((pi/2) - turn_abs) / (pi/2)
+            
+        #set end v for turn to next waypoint
+        vx1 = 0.0
+        vy1 = 0.0
+        if distN > 0.01: #final waypoint edge case
+            vx1 = (lv_max * turn_scale) * (disNX / distN)
+            vy1 = (lv_max * turn_scale) * (disNY / distN)
+        gEndVel = sqrt((vx1**2) + (vy1**2))
+
+        #3rd order stuff guessing hard here.
+        T = max(1.5, dist / 0.20)   # 0.20 m/s target speed
+        dt = 0.1
+        vx0, vy0 = self.prev_vx, self.prev_vy
+        x0 = x
+        y0 = y
+        xT = gx
+        yT = gy
+        
+        #poly time scale
+        ax0, ax1, ax2, ax3 = self.polynomial_time_scaling_3rd_order(x0, vx0, xT, vx1, T)
+        ay0, ay1, ay2, ay3 = self.polynomial_time_scaling_3rd_order(y0, vy0, yT, vy1, T)
+        
+        t = 0.0
+        while not rospy.is_shutdown() and t <= T:
+            #get pose from Pose2D
+            x, y, th = self.pose.x, self.pose.y, self.pose.theta
+            
+            #use poly time scale thingy
+            # polynomial moving target at time t
+            x_des = ax0 + ax1*t + ax2*(t**2) + ax3*(t**3)
+            y_des = ay0 + ay1*t + ay2*(t**2) + ay3*(t**3)
+
+            disX = x_des - x
+            disY = y_des - y
+            dist = sqrt(disX**2 + disY**2)
+            
+            #Now we do PID stuff
+            #calculate desired theta towards goal point
+            gth = atan2(disY, disX)
+            ang_err = gth - th
+            while ang_err > pi:
+                ang_err -= 2*pi
+            while ang_err < -pi:
+                ang_err += 2*pi
+                
+                
+            cont.setPoint(gth)
+            #PID output
+            w = cont.update(th, True)
+            
+            #prevent glitches with max angular vel
+            gdist = euclid_dist((x,y),current_waypoint)
+            if w > av_max:
+                w = av_max
+            elif w < -av_max:
+                w = -av_max
+            #set linear vel
+            if abs(ang_err) > 0.5:
+                v = 0.0
+            elif abs(ang_err) > 0.2 and gdist > 0.2:
+                v = 0.1 #half max speed
+            elif gdist <= 0.2 or (T - t) <= 0.3:
+                v = gEndVel #the desired end speed.
+            else:
+                v = 0.22 #max linear speed
+            
+            vel = Twist() #vel msg
+            vel.linear.x = v
+            vel.linear.y = 0.0
+            vel.linear.z = 0.0
+            vel.angular.x = 0.0
+            vel.angular.y = 0.0
+            vel.angular.z = w
+            self.vel_pub.publish(vel)
+            self.rate.sleep()
+            t += dt
+        self.vel_pub.publish(Twist())
+        self.prev_vx = vx1
+        self.prev_vy = vy1
 
 
     def polynomial_time_scaling_3rd_order(self, p_start, v_start, p_end, v_end, T):
